ffd89402be05d7eb192be408508dcd0036f98c04
67gjy <1963731005@qq.com>
2022-09-03 16:40:52 +0800

    Changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import store from  '@/store'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'ManageView',
+    component: () => import('../views/ManageView'),
+    redirect: "/home",
+    children:[
+      {path: 'home', name: '首页', component: () => import('../views/HomeView')},
+      {path: 'user', name: '用户管理', component: () => import('../views/UserView')},
+      {path: 'Person', name: '个人信息', component: () => import('../views/PersonView')},
+      {path: 'file', name: '文件管理', component: () => import('../views/FileView')}
+    ]
   },
   {
     path: '/about',
@@ -17,6 +24,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'LoginView',
+    component: () => import( '../views/LoginView')
+  },
+  {
+    path: '/register',
+    name: 'RegisterView',
+    component: () => import( '../views/RegisterView')
   }
 ]
 
@@ -24,6 +41,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+
+router.beforeEach((to,from,next) => {
+  localStorage.setItem("currentPathName",to.name)
+  store.commit("setPath")
+  next()
 })
 
 export default router
---
+++ src/views/ManageView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div style="height: 100%">
+    <el-container style="height: 100%; border: 1px solid #eee">
+
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246); height: 100%;">
+        <AsideMall></AsideMall>
+      </el-aside>
+
+      <el-container>
+        <el-header style="text-align: left; font-size: 12px;border-bottom: 1px solid #ccc;line-height: 60px" >
+          <head-mall :user =user></head-mall>
+        </el-header>
+
+        <el-main>
+          <router-view @refreshUser="getUser"/>
+        </el-main>
+
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+import AsideMall from "@/components/AsideMall";
+import HeadMall from "@/components/HeadMall";
+export default {
+  components: {HeadMall, AsideMall},
+  data() {
+    return {
+      user: {}
+    }
+  },
+  created() {
+    this.getUser()
+  },
+  methods: {
+    //拿user数据
+    getUser(){
+      let username = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")).username : ""
+      this.request.get("/user/username/" + username).then(res => {
+        //重新赋值
+        this.user = res.data
+      })
+    },
+  }
+
+};
+</script>
